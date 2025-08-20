37cb28c5efa97ee1233454b1450722632f31fcc8
Lars Andreas Magnussen <lars.magnussen@kvalitetskontroll.a>
2025-08-20 15:52:02 +0200

    add routes for task list endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectApiController;
+use App\Http\Controllers\Api\TaskListApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('/project', ProjectApiController::class);
+Route::apiResource('/project.task-list', TaskListApiController::class);
